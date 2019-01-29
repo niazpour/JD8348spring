11b40148feb80d426c77d292b5204de947918417
acouvillon <auburn2678@gmail.com>
2019-01-28 20:20:58 -0500

    First test of android app

---
+++ app/src/main/java/com/example/roshnighosh/costoutbreaktool/HomePage.java
@@ -0,0 +1,39 @@
+package com.example.roshnighosh.costoutbreaktool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        final Button loginButton = findViewById(R.id.logInButton);
+        final Button signupButton = findViewById(R.id.signUpButton);
+        //final CheckedTextView registerView = findViewById(R.id.checkedTextView);
+        //imageView = (ImageView) findViewById(R.id.imageView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(HomePage.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent1 = new Intent(HomePage.this, SignUpPage.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage.java
@@ -0,0 +1,25 @@
+package com.example.roshnighosh.costoutbreaktool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+
+//        final Button backButton = findViewById(R.id.backButton);
+//        backButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent2 = new Intent(SignUpPage.this, HomePage.class);
+//                startActivity(intent2);
+//            }
+//        });
+    }
+}
