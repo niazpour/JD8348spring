87eed8fdaa261cfe1805348584a92e26563b3357
acouvillon <auburn2678@gmail.com>
2019-01-28 21:13:02 -0500

    all sign up pages done

--- app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage2.java
+++ app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage2.java
@@ -1,23 +1,27 @@
 package com.example.roshnighosh.costoutbreaktool;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class SignUpPage extends AppCompatActivity {
+/**
+ * Created by Abby on 1/28/19.
+ */
+
+public class SignUpPage2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.activity_sign_up_page2);
 
         final Button nextButton = findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(SignUpPage.this, SignUpPage2.class);
+                Intent intent2 = new Intent(SignUpPage2.this, SignUpPage3.class);
                 startActivity(intent2);
             }
         });
--- app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage3.java
+++ app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage3.java
@@ -1,23 +1,27 @@
 package com.example.roshnighosh.costoutbreaktool;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class SignUpPage extends AppCompatActivity {
+/**
+ * Created by Abby on 1/28/19.
+ */
+
+public class SignUpPage3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.activity_sign_up_page3);
 
-        final Button nextButton = findViewById(R.id.nextButton);
+        final Button nextButton = findViewById(R.id.finishButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(SignUpPage.this, SignUpPage2.class);
+                Intent intent2 = new Intent(SignUpPage3.this, MainPage.class);
                 startActivity(intent2);
             }
         });
