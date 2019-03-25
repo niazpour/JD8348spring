a727157d6ddf4fd77a6752bef8a1f78d61b10e7e
acouvillon <auburn2678@gmail.com>
2019-03-24 20:18:18 -0400

    set up connection to database on SignUpPage
    The current changes connect to a local database I have hosted on my laptop. Will update when I have access to the gatech database

--- app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage.java
+++ app/src/main/java/com/example/roshnighosh/costoutbreaktool/SignUpPage.java
@@ -1,6 +1,7 @@
 package com.example.roshnighosh.costoutbreaktool;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class SignUpPage extends AppCompatActivity {
@@ -21,8 +27,13 @@ public class SignUpPage extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String jdbcUrl = "jdbc:mysql://10.0.2.2:3306/mydb";
+                new SignUpDB().execute(jdbcUrl);
+
                 Intent intent2 = new Intent(SignUpPage.this, SignUpPage2.class);
                 startActivity(intent2);
+
             }
         });
 
@@ -40,5 +51,35 @@ public class SignUpPage extends AppCompatActivity {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+
+    private static class SignUpDB extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String[] objects) {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch(ClassNotFoundException ex) {
+                System.err.println("Unable to load MySQL Driver");
+            }
+            System.out.println(Thread.currentThread().getName());
+
+            try(Connection con = DriverManager.getConnection(objects[0], "root", "M65IUaa5cx")) {
+                System.out.println("Connected!");
+                String queryString = "select version()";
+                Statement stmt = con.createStatement();
+                ResultSet rset = stmt.executeQuery(queryString);
+                while ( rset.next()) {
+                    System.out.println("Version: " + rset.getString(1));
+                }
+
+                rset.close();
+                stmt.close();
+            } catch(SQLException e) {
+                System.out.println("Failed");
+            }
+            return null;
+        }
     }
 }
